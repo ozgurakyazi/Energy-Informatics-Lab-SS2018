b2360bc1ca48f89a89399509bbf0c67b53531fa1
Ozgur Akyazi <ozgur.akyazi@boun.eud.tr>
2018-07-06 21:54:33 +0300

    critical times for input. Not used

--- 6-Data_Prep_and_Vis/app.py
+++ 6-Data_Prep_and_Vis/app.py
@@ -21,6 +21,7 @@ class Blond(object):
     def __init__(self, date, day_data = {}):
         self.date = date
         self._day_data = day_data
+        self.time_stamps = {}
 
 
     def list_files(self):
@@ -37,6 +38,17 @@ class Blond(object):
         return list(filter_list)
 
 
+    def find_corresponding_file(self, the_time,all_timestamps):
+        current_ts = all_timestamps[0]
+        i=1
+        try:
+            while the_time >= current_ts:
+                current_ts = all_timestamps[i]
+                i+=1
+        except: # in case requested time is in the last index of the list, we will have a error in [i]. so this except makes the index work.
+            i+=1
+
+        return i-2
     def _read_files_from_folder(self, files, start_ts, end_ts,path_to_files):
         """This method gets file in the folder w.r.t. to the timeframe start_ts - end_ts
 
@@ -58,10 +70,13 @@ class Blond(object):
         while start_ts >= current_ts:
             current_ts = timestamps[i]
             i+=1
+        file_index = self.find_corresponding_file(start_ts,timestamps)
+        if file_index < 0: #no file found
+            return [],[]
 
         """add first file timestamp to the rest"""
-        timestamps = [timestamps[i-2]] + [ts for ts in timestamps if start_ts <= ts <= end_ts]
-        timestamps_filter= map(lambda ts: datetime.combine(self.date, ts).strftime(time_format), list(timestamps))
+        res_timestamps = [timestamps[file_index]] + [ts for ts in timestamps if start_ts <= ts <= end_ts]
+        timestamps_filter= map(lambda ts: datetime.combine(self.date, ts).strftime(time_format), list(res_timestamps))
 
         res_list = []
         for ts in timestamps_filter:
@@ -76,7 +91,7 @@ class Blond(object):
                 temp_file.close()
                 data.append(h5py.File(path_to_files + a_file,'r+'))
 
-        return data
+        return data , res_timestamps
 
 
     def read_files(self, start_ts, end_ts):
@@ -92,7 +107,7 @@ class Blond(object):
         """READING CLEAR UNIT"""
         path_to_clear = './data/clear/'
         files_all = next(os.walk(path_to_clear))[2]
-        self._day_data['clear'] = self._read_files_from_folder(files_all, start_ts, end_ts,path_to_clear)
+        self._day_data['clear'], self.time_stamps["clear"] = self._read_files_from_folder(files_all, start_ts, end_ts,path_to_clear)
         #self._day_data['clear'] = [h5py.File(path_to_clear + file_name,'r+') for file_name in target_files]
 
 
@@ -103,7 +118,7 @@ class Blond(object):
         for folder in glob.glob(path_to_medals):
             files_all = next(os.walk(folder))[2]
             medal_name = re.search(r'(medal-\d+)', folder).group(1)
-            self._day_data[medal_name] = self._read_files_from_folder(files_all, start_ts, end_ts,folder)
+            self._day_data[medal_name], self.time_stamps[medal_name] = self._read_files_from_folder(files_all, start_ts, end_ts,folder)
             #self._day_data[medal_name] = [h5py.File(folder + file_name,'r+') for file_name in target_files]
 
 
@@ -161,11 +176,18 @@ class Blond(object):
 
         return data_calibrated
 ##########################
+def get_time_diff(t1,t2):
+    t1_s = (t1.hour*60*60 + t1.minute*60 + t1.second)
+    t2_s = (t2.hour*60*60 + t2.minute*60 + t2.second)
+
+    delta_s = max([t1_s, t2_s]) - min([t1_s, t2_s])
+    return delta_s
+##########################
 blond = Blond(date(2016,10,5))
 
 """ Define a timeframe"""
 start_ts = time(0,10,0) # start_hours_minutes
-end_ts   = time(0,10,10)
+end_ts   = time(1,10,10)
 
 """Read MEDAL and CLEAR data """
 blond.read_files(start_ts, end_ts)
@@ -199,6 +221,32 @@ print(dict_signal)
 blond.center_and_calibrate(dict_signal)
 ##########################
 
+def create_options():
+    seconds = get_time_diff(start_ts,end_ts)
+    minutes = int(seconds/60)
+    hours = int(minutes/60)
+
+    options={"seconds":[],"minutes":[],"hours":[], "critical":{"minutes":[],"seconds":[]} }
+    if seconds>59:
+        options["seconds"] = [{"label":i,"value":i} for i in range(60)]
+        if minutes>59:
+            options["minutes"] = [{"label":i,"value":i} for i in range(60)]
+            resid_min = minutes % 60
+            options["critical"]["minutes"] = [{"label":i,"value":i} for i in range()]
+        else:
+            options["minutes"] = [{"label":i,"value":i} for i in range(start_ts.minute,start_ts.minute+minutes+1)]
+            options["critical"]["minutes"] = options["minutes"]
+
+        options["hours"] = [{"label":i,"value":i} for i in range(start_ts.hour,start_ts.hour+hours+1)]
+    else:
+        options["seconds"] = [{"label":i,"value":i} for i in range(start_ts.second,start_ts.second+seconds+1)]
+        options["critical"]["seconds"] = options["seconds"]
+        options["minutes"] = [{"label":start_ts.minute,"value":start_ts.minute} ]
+        options["critical"]["minutes"] = options["minutes"]
+        options["hours"] = [{"label":start_ts.hour,"value":start_ts.hour} ]
+    return options, criticals
+
+time_options,critical_times = create_options()
 ##########################
 
 app = dash.Dash()
@@ -206,15 +254,12 @@ app = dash.Dash()
 app.layout = html.Div(children=[
     html.H3(children='BLOND Dataset'),
 
-    # html.Div(children='''
-    #     Dash: A web application framework for Python.
-    # '''),
     html.Div([
         html.Div([
             html.Div([
                 dcc.Dropdown(
                     id="hour",
-                    options=[{"label":i,"value":i} for i in range(24)],
+                    options=time_options["hours"],
                     value = "Start Hour:",
                 ),
                 ],
@@ -223,7 +268,7 @@ app.layout = html.Div(children=[
             html.Div([
                 dcc.Dropdown(
                     id="minute",
-                    options=[{"label":i,"value":i} for i in range(60)],
+                    options=time_options["minutes"],
                     value = "Start Minute:",
                 ),
                 ],
@@ -232,42 +277,52 @@ app.layout = html.Div(children=[
             html.Div([
                 dcc.Dropdown(
                     id="second",
-                    options=[{"label":i,"value":i} for i in range(60)],
+                    options=time_options["seconds"],
                     value = "Start Seconds:",
                 ),
                 ],
                 style={"display":"inline-block", "width":"33%"}
             ),
-            dcc.Dropdown(
-                id='graph_type',
-                options=[{"label":"Aggregated Signals","value":0},{"label":"Individual Signals","value":1}],
-                value=0,
-            ),
+
         ],
         style= {"width":"30%", "display":"inline-block"}
         ),
         html.Div([
-            dcc.Dropdown(
-                id="phases",
-                value = 1,
+            html.Div([
+                dcc.Dropdown(
+                    id='graph_type',
+                    options=[{"label":"Aggregated Signals","value":0},{"label":"Individual Signals","value":1}],
+                    value=0,
+                ),
+            ],
+            style={"display":"inline-block", "width":"15%"}
+            ),
+            html.Div([
+                dcc.Dropdown(
+                    id="phases",
+                    value = 1,
+                ),
+            ],
+            style={"display":"inline-block", "width":"15%"}
             ),
         ],
-        style= {"width":"30%", "display":"inline-block"}
         )
         ],
     ),
-    dcc.RangeSlider(
-        id="timerange",
-        count=1,
-        min=-5,
-        max=10,
-        step=0.5,
-        value=[-3, 7]
-    ),
+    # dcc.RangeSlider(
+    #     id="timerange",
+    #     count=1,
+    #     min=-5,
+    #     max=10,
+    #     step=0.5,
+    #     value=[-3, 7]
+    # ),
     dcc.Graph(
         id='graph',
     )
 ])
+
+
 
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
@@ -297,7 +352,32 @@ def update_phases_options(graph_type):
         Input("second","value"),
     ]
 )
+@app.callback(
+    Output("minute", "options"),
+    [
+        Input("hour","value"),
+    ]
+)
+def update_minute_options(hour):
+    if hour == critical_times["hour"]:
+
+    else:
+        return time_options
+
+@app.callback(
+    Output("graph", "figure"),
+    [
+        Input("graph_type","value"),
+        Input("phases", "value"),
+        Input("hour","value"),
+        Input("minute","value"),
+        Input("second","value"),
+    ]
+)
 def update_graph(graph_type,phase,hour,minute,second):
+
+    if not(type(hour) == int and type(minute)==int and type(second) ==int):
+        return 0
     the_mode= "markers+lines"
     title = ""
     data_fields = ""
@@ -312,9 +392,14 @@ def update_graph(graph_type,phase,hour,minute,second):
         data_fields = [key for key in blond.list_files().keys() if key.startswith("medal")]
         sps = 6400
 
+
+    requested_time = time(hour,minute,second)
     graph_list = []
     for data_field in data_fields:
-        temp_data = blond.list_files()[data_field][0]["current"+str(phase)][0:sps*10]
+        file_index = blond.find_corresponding_file(requested_time, blond.time_stamps[data_field])
+        time_diff = get_time_diff(requested_time , blond.time_stamps[data_field][file_index])
+        data_index_shift = time_diff * sps
+        temp_data = blond.list_files()[data_field][file_index]["current"+str(phase)][data_index_shift:data_index_shift + int(sps*0.1)]
         len_data = temp_data.shape[0]
         print("length of "+data_field + "is:"+str(len_data))
         print()
